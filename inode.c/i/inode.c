126f2e2928644294c24cc39c507030299b6134af
shinbaad <shinbaad@gmail.com>
2021-03-17 11:15:33 +0900

    add inode.c

---
+++ inode.c/i/inode.c
@@ -0,0 +1,87 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <inode.h>
+
+
+int32_t SocketCreate()
+{
+  return 0;
+}
+
+
+int32_t SocketDestroy()
+{
+  return 0;
+}
+
+
+int32_t SocketWrite()
+{
+  return 0;
+}
+
+int32_t SocketRead()
+{
+  return 0;
+}
+
+
+
+
+
+
+
+
+
+int32_t SerialCreate()
+{
+  return 0;
+}
+
+
+int32_t SerialDestroy()
+{
+  return 0;
+}
+
+
+int32_t SerialWrite()
+{
+  return 0;
+}
+
+int32_t SerialRead()
+{
+  return 0;
+}
+
+
+
+
+
+
+
+
+int32_t HttpCreate()
+{
+  return 0;
+}
+
+
+int32_t HttpDestroy()
+{
+  return 0;
+}
+
+
+int32_t HttpWrite()
+{
+  return 0;
+}
+
+int32_t HttpRead()
+{
+  return 0;
+}
+
---
+++ inode.c/i/inode.h
@@ -0,0 +1,35 @@
+/*****************************************************************************/
+/*                                                                           */
+/*            DVLab (Data Visualization Lab) CORE version 1.0                */
+/*                                                                           */
+/*****************************************************************************/
+/*****************************************************************************/
+/*                                                                           */
+/*  File Name         : inode.h                                              */
+/*                                                                           */
+/*  Description       :                                                      */
+/*                                                                           */
+/*                                                                           */
+/*  Issues / Problems : None                                                 */
+/*                                                                           */
+/*  Revision History  :                                                      */
+/*                                                                           */
+/*        DD MM YYYY   Author(s)        Changes (Describe the changes made)  */
+/*        17 03 2021   Shin Seunghyeok  Draft                                */
+/*                                                                           */
+/*****************************************************************************/
+#ifndef __INODE_H_F2DBDC40_6196_4E67_A689_D31A9310BEC0__
+#define __INODE_H_F2DBDC40_6196_4E67_A689_D31A9310BEC0__
+
+#if defined WIN32
+#define delay(a)  Sleep(a)
+#endif
+#if defined LINUX
+#define delay(a)  usleep(a)
+#endif
+
+
+
+
+
+#endif
